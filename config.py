3ababf293f469dd3218b3ef70707e3e49861657c
GAURI-MUSIC-REPO <gaurishankar143666@gmail.com>
2025-12-02 12:52:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,15 +36,15 @@ API_KEY = getenv("API_KEY", 'NxGBNexGenBots254c6b') # youtube song api ke
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ISTKHAROP/IstkharMusic",
+    "https://github.com/GAURI-MUSIC-REPO/MUSIC-BOT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/best_dealer_hub")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+XIsACwwVfYVjMzE1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/GSKCHANNEL666")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/StudyGroup2099")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -121,6 +121,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
